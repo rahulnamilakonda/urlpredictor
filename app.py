568d11f7869a929dbff973bc0800679c7913abb6
namilakonda rahul <rahulnamilakonda100@gmail.com>
2022-12-26 23:04:23 +0530

    inital

---
+++ app.py
@@ -0,0 +1,27 @@
+# Flask API creation
+
+import pickle
+
+from flask import Flask, request ,jsonify
+# import logisticmodel
+
+# unpickling the model
+
+model = pickle.load(open('model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    return "hello world"
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    url = request.form.get('url')
+    res = model.predict([url])
+    return jsonify({'result':str(res)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
